ea0a99f2653aeb58df036b8b23361cd3ebdde806
pcboy <david.hagege@gmail.com>
2015-05-23 16:29:22 +0900

    Add config file

---
+++ config.rb
@@ -0,0 +1,34 @@
+# -*- encoding : utf-8 -*-
+#
+#          DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                  Version 2, December 2004
+#
+#  Copyright (C) 2004 Sam Hocevar
+#  14 rue de Plaisance, 75014 Paris, France
+#  Everyone is permitted to copy and distribute verbatim or modified
+#  copies of this license document, and changing it is allowed as long
+#  as the name is changed.
+#  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+#
+#
+#  David Hagege <david.hagege@gmail.com>
+#
+
+PRODUCTS = ['irobot']
+TO_EMAIL =  'your@email.com'
+
+mail_config = { :address              => "smtp.gmail.com",
+                :port                 => 587,
+                :domain               => 'your.host.name',
+                :user_name            => '<username>',
+                :password             => '<password>',
+                :authentication       => 'plain',
+                :enable_starttls_auto => true  }
+
+
+
+Mail.defaults do
+  delivery_method :smtp, mail_config
+end
